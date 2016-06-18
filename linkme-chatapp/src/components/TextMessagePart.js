70cdac9d2efbfb509fd5e0839e07ddcacd3423a0
Vando Pereira <vape@microsoft.com>
2016-06-18 20:01:45 +0200

    Fixes #22 - make link messages, linkable

--- linkme-chatapp/src/components/TextMessagePart.js
+++ linkme-chatapp/src/components/TextMessagePart.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 export default class TextMessagePart extends Component {
   render() {
+    if(this.props.messagePart.body.indexOf("http") > -1) {
+      return <div className='bubble text'><a target="_blank" href={this.props.messagePart.body}>{this.props.messagePart.body}</a></div>;      
+    }
     return <div className='bubble text'>{this.props.messagePart.body}</div>;
   }
 }
